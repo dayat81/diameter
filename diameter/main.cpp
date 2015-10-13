ea8b75f4e31e2b091661f2371225da551cc65f7e
Hidayat Hidayat <uda.dayat@gmail.com>
2015-10-14 01:15:43 +0700

    copy byte in to byte out

--- diameter/main.cpp
+++ diameter/main.cpp
@@ -31,6 +31,12 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
+     int reuseaddr = 1; /* True */
+    /* Enable the socket to reuse the address */
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1) {
+        perror("setsockopt");
+        return 1;
+    }
     if (bind(sockfd, (struct sockaddr *) &serv_addr,
              sizeof(serv_addr)) < 0)
         error("ERROR on binding");
@@ -41,7 +47,7 @@ int main(int argc, char *argv[])
                        &clilen);
     if (newsockfd < 0)
         error("ERROR on accept");
-    bzero(buffer,4);
+    //bzero(buffer,4);
     n = read(newsockfd,buffer,4);
     if (n < 0) error("ERROR reading from socket");
     char* b=buffer;
@@ -51,10 +57,14 @@ int main(int argc, char *argv[])
         i++;
         b++;
     }
-    printf("\n");
+    b=b-4;
+    //printf("\n");
     int32_t l =(((0x00 & 0xff) << 24) | ((buffer[1] & 0xff) << 16)| ((buffer[2] & 0xff) << 8) | ((buffer[3] & 0xff)))-4;
-    printf("len: %zu\n",l);
+    //printf("len: %zu\n",l);
     char next[l];
+    
+    char resp[l+4];
+    char* r=resp;
     //bzero(next, l);
     n = read(newsockfd,next,l);
     if (n < 0) error("ERROR reading from socket");
@@ -65,8 +75,38 @@ int main(int argc, char *argv[])
         i++;
         p++;
     }
+    p=p-l;
      printf("\n");
-    n = write(newsockfd,"I got your message",18);
+    
+    i=0;
+    while (i<4) {
+        *r=*b;
+        r++;
+        b++;
+        i++;
+    }
+    i=0;
+    r=r-4;
+    while (i<4) {
+        printf("%02X ",*r&0xff);
+        i++;
+        r++;
+    }
+    i=0;
+    while (i<l) {
+        *r=*p;
+        p++;
+        i++;
+        r++;
+    }
+    r=r-l;
+    i=0;
+    while (i<l) {
+        printf("%02X ",*r&0xff);
+        r++;
+        i++;
+    }
+    n = write(newsockfd,resp,l+4);
     if (n < 0) error("ERROR writing to socket");
     close(newsockfd);
     close(sockfd);
