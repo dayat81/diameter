de146faf6915c293db7d7472c86c43d330154db7
Hidayat Hidayat <uda.dayat@gmail.com>
2015-10-14 00:11:05 +0700

    Initial Commit

---
+++ diameter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  diameter
+//
+//  Created by hidayat on 10/14/15.
+//  Copyright © 2015 hidayat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
